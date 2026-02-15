f18f059d633b771892a11bcaa00afe1ae6d04447
Mister SchtieF <lischke@gmail.com>
2026-02-15 17:42:12 +0100

    run probs fixed

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,22 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val project = this
+    plugins.whenPluginAdded {
+        // Wir prüfen, ob es ein Android-Plugin ist (Library oder App)
+        if (this::class.java.name.contains("com.android.build.gradle.LibraryPlugin") || 
+            this::class.java.name.contains("com.android.build.gradle.AppPlugin")) {
+            
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace == null) {
+                // Wir setzen den Namespace direkt aus der Projektgruppe (z.B. com.example.isar)
+                android.namespace = project.group.toString()
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
